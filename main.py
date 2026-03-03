6a0e4fd6f894a2bab62a8bcaa677e42786ab68c5
_skinwalker <kanvoid@yandex.ru>
2026-03-03 21:47:21 +0300

    Changed standardization logic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from src.dataset import preprocess_images
+from data.load_data import import_data
+import os
+from src.config import TRAIN_CATS_DIRECTORY, TEST_CATS_DIRECTORY, TRAIN_DOGS_DIRECTORY, TEST_DOGS_DIRECTORY
+
 def main():
-    print("main")
+    preprocess_images(TRAIN_CATS_DIRECTORY, 128, 128)
     
 if __name__ == "__main__":
-    main()
+    
--- src/dataset.py
+++ src/dataset.py
@@ -1,24 +1,49 @@
 import cv2
+import torch
+from torchvision import transforms
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from src.config import CATS_DIRECTORY, DOGS_DIRECTORY, TRAIN_CATS_DIRECTORY, TEST_CATS_DIRECTORY, TRAIN_DOGS_DIRECTORY, TEST_DOGS_DIRECTORY
+from src.config import CATS_DIRECTORY, DOGS_DIRECTORY, TRAIN_CATS_DIRECTORY, TEST_CATS_DIRECTORY, TRAIN_DOGS_DIRECTORY, TEST_DOGS_DIRECTORY, JPG_EXTENSION_FILTER
 
-img = cv2.imread(os.path.join("dataset", "train", "cats", "1.jpg"), cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def standardize_size_of_image(img_path: str, width: int, height: int):
+    img = cv2.imread(img_path)
+    if img is None:
+        print(f"Неудалось загрузить изображение {img_path}")
+        return None
 
-def standartify_sizes_of_images(images_list: list, width: int, height: int, class_subset_dir: str):
-    for img_name in images_list:
-        img_path = os.path.join(class_subset_dir, img_name)
+    if width < img.shape[1]:
+        resized_img = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
+    else:
+        resized_img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
 
-        img = cv2.imread(img_path)
+    return resized_img
 
-        if width < img.shape[1]:
-            resized_img = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-        else:
-            resized_img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
-        
-        cv2.imwrite(img_path, resized_img)
+def to_tensor(img):
+    transform = transforms.ToTensor()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_tensor = transform(img_rgb)
+
+    return img_tensor
+
+def normalize_image(img_tensor):
+    mean = img_tensor.mean()
+    std = img_tensor.std()
+    normalized_tensor = (img_tensor - mean) / std
+
+    return normalized_tensor
+
+def preprocess_images(class_subset_dir: str, width: int, height: int):
+    img_list = [
+        img for img in os.listdir(class_subset_dir)
+        if img.endswith(JPG_EXTENSION_FILTER) and os.path.isfile(class_subset_dir, img)
+    ]
+
+    img_path = os.path.join(class_subset_dir, img)
+
+    img = standardize_size_of_image(img_path, width, height)
+    img = to_tensor(img)
+    img = normalize_image(img)
+
+    
