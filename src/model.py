8805ea6a08e2fbfd467a89c2db2dd280aadbf0aa
_skinwalker <kanvoid@yandex.ru>
2026-03-05 00:04:59 +0300

    Created CNN Pipeline

---
+++ src/model.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel=3, stride=1, padding=1):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel, stride, padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class CatDogCNN(nn.Module):
+    def __init__(self, num_classes: int = 2):
+        super().__init__()
+
+        # Feature extractor
+        self.features = nn.Sequential(
+            ConvBlock(3, 32),
+            ConvBlock(32, 32),
+            nn.MaxPool2d(2),
+
+            ConvBlock(32, 64),
+            ConvBlock(64, 64),  
+            nn.MaxPool2d(2),
+
+            ConvBlock(64, 128),
+            nn.MaxPool2d(2)
+        )
+
+        # Global pooler
+        self.global_pool = nn.AdaptiveAvgPool2d(1, 1)
+
+        # Classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.global_pool(x)
+        x = self.classifier(x)
+        return x
